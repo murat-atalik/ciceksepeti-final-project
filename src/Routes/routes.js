c52f3df2d55533dbad5b710dade7df28f8598844
Murat ATALIK <62357472+murat-atalik@users.noreply.github.com>
2021-10-25 17:12:17 +0300

    Day 15 commits
    base url fix #3

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -1,11 +1,11 @@
 import { lazy } from 'react';
 
-const Products = lazy(() => import('pages/Products/Products'));
-const Product = lazy(() => import('pages/Product/Product'));
-const AddProduct = lazy(() => import('pages/AddProduct/AddProduct'));
-const Account = lazy(() => import('pages/Account/Account'));
-const Signup = lazy(() => import('pages/Signup/Signup'));
-const Signin = lazy(() => import('pages/Signin/Signin'));
+const Products = lazy(() => import('../pages/Products/Products'));
+const Product = lazy(() => import('../pages/Product/Product'));
+const AddProduct = lazy(() => import('../pages/AddProduct/AddProduct'));
+const Account = lazy(() => import('../pages/Account/Account'));
+const Signup = lazy(() => import('../pages/Signup/Signup'));
+const Signin = lazy(() => import('../pages/Signin/Signin'));
 export const protectedRoutes = [
   { path: '/account', component: Account, exact: false },
   { path: '/add-product', component: AddProduct, exact: false },
